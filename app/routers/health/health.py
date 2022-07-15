81c93247a471fc5b89a8a78a04a32700c4b9ec6e
gregmccoy <greg@gkmc.ca>
2022-07-15 09:46:07 -0400

    Adding logging

--- app/routers/health/health.py
+++ app/routers/health/health.py
@@ -7,7 +7,10 @@ from app.models.sql_invitation import InvitationModel
 from sqlalchemy import create_engine
 from app.resources.error_handler import APIException
 from app.config import ConfigSettings
+from common import LoggerFactory
 
+
+logger = LoggerFactory('api_health').get_logger()
 
 router = APIRouter(tags=["Health"])
 
@@ -16,16 +19,22 @@ def psql_health_check() -> bool:
     try:
         create_engine(ConfigSettings.RDS_DB_URI, echo=True)
     except Exception as e:
-        raise APIException(error_msg=f"Couldn't connect to postgres: {e}", status_code=503)
+        error_msg = f"Couldn't connect to postgres: {e}"
+        logger.error(error_msg)
+        raise APIException(error_msg=error_msg, status_code=503)
 
     try:
         db.session.query(UserEventModel).first()
     except Exception as e:
-        raise APIException(error_msg=f"Couldn't query user_event table: {e}", status_code=503)
+        error_msg = f"Couldn't query user_event table: {e}"
+        logger.error(error_msg)
+        raise APIException(error_msg=error_msg, status_code=503)
     try:
         db.session.query(InvitationModel).first()
     except Exception as e:
-        raise APIException(error_msg=f"Couldn't query invitation table: {e}", status_code=503)
+        error_msg = f"Couldn't query invitation table: {e}"
+        logger.error(error_msg)
+        raise APIException(error_msg=error_msg, status_code=503)
     return True
 
 
@@ -36,5 +45,6 @@ class Health:
         summary="Health check",
     )
     async def get(self):
+        logger.info("Starting health checks")
         psql_health_check()
         return Response(status_code=204)
